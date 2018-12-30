a11c49d8e6b92b037fb953f29880e5b9c9744179
Marcelo Abrão da Silva <masilvasql@gmail.com>
2018-12-29 22:06:19 -0200

    deteccao hog

--- deteccao_face_hog2.py
+++ deteccao_face_hog2.py
@@ -4,9 +4,9 @@ import dlib
 subdetector = ["Olhar a frente", "Vista a esquerda", "Vista a direita",
                "A frente girando a esquerda", "A frente girando a direita"]
 
-imagem = cv2.imread("fotos/grupo.0.jpg")
+imagem = cv2.imread("fotos/teste.3.jpg")
 detector = dlib.get_frontal_face_detector()
-facesDetectadas, pontuacao, idx = detector.run(imagem, 1, -2)
+facesDetectadas, pontuacao, idx = detector.run(imagem, 5    ,-0.5)
 #print(facesDetectadas)
 #print(pontuacao)
 #print(idx)
@@ -15,7 +15,7 @@ for i, d in enumerate(facesDetectadas):
     #print(d)
     print("Detecção: {}, pontuação: {}, Sub-detector: {}".format(d, pontuacao[i], idx[i]))
     e, t, d, b = (int(d.left()), int(d.top()), int(d.right()), (int(d.bottom())))
-    
+
     cv2.rectangle(imagem, (e,t), (d,b), (0,255,0) , 2)
 
 cv2.imshow("Detector HOG 2", imagem)
